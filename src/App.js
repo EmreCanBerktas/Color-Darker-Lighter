2585ea8d350436f4bcca85c4569b567817aeba63
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-08-01 16:20:38 +0300

    ui updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Heading, Box, Input, Flex, Container } from "@chakra-ui/react";
+import { Heading, Input, Flex, Container } from "@chakra-ui/react";
 import Card from "./Card";
 import { useState } from "react";
 function App() {
-  const [color, setColor] = useState("#FFC0CB");
+  const [color, setColor] = useState("#FF7000");
   return (
     <div>
       <Heading>Color Shader</Heading>
@@ -22,94 +22,25 @@ function App() {
           </Flex>
           <br></br>
           <br></br>
-          <Flex alignItems="center" justifyContent="space-around">
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %0
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %10
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %20
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %30
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %40
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %50
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %60
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %70
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %80
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %90
-            </Box>
-            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
-              %100
-            </Box>
-          </Flex>
-          <br></br>
           <div
             style={{
               display: "flex",
               flexDirection: "row-reverse",
               justifyContent: "center",
-            }}
-          >
-            <Card color={color} number={-100} />
-            <Card color={color} number={-90} />
-            <Card color={color} number={-80} />
-            <Card color={color} number={-70} />
-            <Card color={color} number={-60} />
-            <Card color={color} number={-50} />
-            <Card color={color} number={-40} />
-            <Card color={color} number={-30} />
-            <Card color={color} number={-20} />
-            <Card color={color} number={-10} />
-            <Card color={color} number={0} />
-          </div>
-          <br></br>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Card color={color} number={0} />
-            <Card color={color} number={10} />
-            <Card color={color} number={20} />
-            <Card color={color} number={30} />
-            <Card color={color} number={40} />
-            <Card color={color} number={50} />
-            <Card color={color} number={60} />
-            <Card color={color} number={70} />
-            <Card color={color} number={80} />
-            <Card color={color} number={90} />
-            <Card color={color} number={100} />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
               alignItems: "center",
-              flexDirection: "column",
             }}
           >
-            {/* <input
-          type="color"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        /> */}
+            <Card color={color} number={100} />
+            <Card color={color} number={90} />
+            <Card color={color} number={80} />
+            <Card color={color} number={70} />
+            <Card color={color} number={60} />
+            <Card color={color} number={50} />
+            <Card color={color} number={40} />
+            <Card color={color} number={30} />
+            <Card color={color} number={20} />
+            <Card color={color} number={10} />
+            <Card color={color} number={0} />
           </div>
         </div>
       </Container>
--- src/Card.js
+++ src/Card.js
@@ -1,39 +1,59 @@
-import { Alert } from "@chakra-ui/react";
+import { Alert, Flex, Box } from "@chakra-ui/react";
 import React from "react";
 import { LightenDarkenColor } from "lighten-darken-color";
 
 function Card({ color, number }) {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: LightenDarkenColor(color, number),
-          color: "white",
-          width: "65px",
-          height: "65px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          cursor: "pointer",
-          padding: "5px",
-        }}
-        onClick={() => {
-          navigator.clipboard.writeText(LightenDarkenColor(color, number));
-          console.log("yolo");
-          <Alert status="success">
-            Hex Code Successfully Copied To Clipboard
-          </Alert>;
-        }}
-      >
-        {LightenDarkenColor(color, number)}
-      </div>
+    <div>
+      <Flex alignItems="center" flexDirection="column">
+        <Box backgroundColor="grey" borderRadius="20%">
+          %{number}
+        </Box>
+        <br></br>
+        <div
+          style={{
+            backgroundColor: LightenDarkenColor(color, number),
+            color: "white",
+            width: "65px",
+            height: "65px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
+            padding: "5px",
+          }}
+          onClick={() => {
+            navigator.clipboard.writeText(LightenDarkenColor(color, number));
+            <Alert status="success">
+              Hex Code Successfully Copied To Clipboard
+            </Alert>;
+          }}
+        >
+          {LightenDarkenColor(color, number)}
+        </div>
+        <br></br>
+        <div
+          style={{
+            backgroundColor: LightenDarkenColor(color, -number),
+            color: "white",
+            width: "65px",
+            height: "65px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
+            padding: "5px",
+          }}
+          onClick={() => {
+            navigator.clipboard.writeText(LightenDarkenColor(color, -number));
+            <Alert status="success">
+              Hex Code Successfully Copied To Clipboard
+            </Alert>;
+          }}
+        >
+          {LightenDarkenColor(color, -number)}
+        </div>
+      </Flex>
     </div>
   );
 }
