fa608faa31e8a497676e90ef6753a6dfc5eb1249
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-08-01 12:03:45 +0300

    wrapped with container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Heading, Box, Input, Flex } from "@chakra-ui/react";
+import { Heading, Box, Input, Flex, Container } from "@chakra-ui/react";
 import Card from "./Card";
 import { useState } from "react";
 function App() {
@@ -6,171 +6,113 @@ function App() {
   return (
     <div>
       <Heading>Color Shader</Heading>
-      <div>
-        <Flex justifyContent="center" alignItems="center">
-          <Input
-            width="400px"
-            value={color}
-            variant="filled"
-            placeholder="Basic usage"
-            size="lg"
-            onChange={(e) => {
-              setColor(e.target.value);
+      <Container maxW="730px">
+        <div>
+          <Flex justifyContent="center" alignItems="center">
+            <Input
+              width="400px"
+              value={color}
+              variant="filled"
+              placeholder="Basic usage"
+              size="lg"
+              onChange={(e) => {
+                setColor(e.target.value);
+              }}
+            />
+          </Flex>
+          <br></br>
+          <br></br>
+          <Flex alignItems="center" justifyContent="space-around">
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %0
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %10
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %20
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %30
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %40
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %50
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %60
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %70
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %80
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %90
+            </Box>
+            <Box bg="#a1a1a1" borderRadius="5px" fontSize="1.25rem">
+              %100
+            </Box>
+          </Flex>
+          <br></br>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row-reverse",
+              justifyContent: "center",
             }}
-          />
-        </Flex>
-        <br></br>
-        <br></br>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="32.5%"
           >
-            %0
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.5%"
+            <Card color={color} number={-100} />
+            <Card color={color} number={-90} />
+            <Card color={color} number={-80} />
+            <Card color={color} number={-70} />
+            <Card color={color} number={-60} />
+            <Card color={color} number={-50} />
+            <Card color={color} number={-40} />
+            <Card color={color} number={-30} />
+            <Card color={color} number={-20} />
+            <Card color={color} number={-10} />
+            <Card color={color} number={0} />
+          </div>
+          <br></br>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
           >
-            %10
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.35%"
+            <Card color={color} number={0} />
+            <Card color={color} number={10} />
+            <Card color={color} number={20} />
+            <Card color={color} number={30} />
+            <Card color={color} number={40} />
+            <Card color={color} number={50} />
+            <Card color={color} number={60} />
+            <Card color={color} number={70} />
+            <Card color={color} number={80} />
+            <Card color={color} number={90} />
+            <Card color={color} number={100} />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
           >
-            %20
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.4%"
-          >
-            %30
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.4%"
-          >
-            %40
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.4%"
-          >
-            %50
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.5%"
-          >
-            %60
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.4%"
-          >
-            %70
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.4%"
-          >
-            %80
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.5%"
-          >
-            %90
-          </Box>
-          <Box
-            bg="#a1a1a1"
-            borderRadius="5px"
-            fontSize="1.25rem"
-            marginLeft="1.3%"
-          >
-            %100
-          </Box>
-        </div>
-        <br></br>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row-reverse",
-            justifyContent: "center",
-          }}
-        >
-          <Card color={color} number={-100} />
-          <Card color={color} number={-90} />
-          <Card color={color} number={-80} />
-          <Card color={color} number={-70} />
-          <Card color={color} number={-60} />
-          <Card color={color} number={-50} />
-          <Card color={color} number={-40} />
-          <Card color={color} number={-30} />
-          <Card color={color} number={-20} />
-          <Card color={color} number={-10} />
-          <Card color={color} number={0} />
-        </div>
-        <br></br>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-          }}
-        >
-          <Card color={color} number={0} />
-          <Card color={color} number={10} />
-          <Card color={color} number={20} />
-          <Card color={color} number={30} />
-          <Card color={color} number={40} />
-          <Card color={color} number={50} />
-          <Card color={color} number={60} />
-          <Card color={color} number={70} />
-          <Card color={color} number={80} />
-          <Card color={color} number={90} />
-          <Card color={color} number={100} />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
-          {/* <input
+            {/* <input
           type="color"
           value={color}
           onChange={(e) => setColor(e.target.value)}
         /> */}
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 }
